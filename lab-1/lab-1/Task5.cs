06d2ca492c258612acd5ef00d3b29f2f663f65f7
l1nehalt <alexdenisov656@gmail.com>
2026-02-16 14:03:59 +0300

    initial

---
+++ lab-1/lab-1/Task5.cs
@@ -0,0 +1,25 @@
+﻿namespace lab_1;
+
+public static class Task5
+{
+    public static int GetDifferenceMaxMin(int[] array)
+    {
+        int max = int.MinValue;
+        int min = int.MaxValue;
+            
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+
+            if (array[i] < min)
+            {
+                min =  array[i];
+            }
+        }
+        
+        return max - min;
+    }
+}
---
+++ lab-1/lab-1/Task6.cs
@@ -0,0 +1,19 @@
+﻿namespace lab_1;
+
+public static class Task6
+{
+    public static int CountElements(int[] m)
+    {
+        int count = 0;
+
+        for (int i = 0; i < m.Length; i++)
+        {
+            if (m[i] > 0 && m[i] < 125)
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
