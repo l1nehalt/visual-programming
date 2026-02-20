fc2e3a3487198e8ef2337ce7aa9b70284771416b
l1nehalt <alexdenisov656@gmail.com>
2026-02-20 14:57:17 +0300

    add GetHashCode

--- lab-2/lab-2/obj/Debug/net8.0/lab-2.AssemblyInfo.cs
+++ lab-2/lab-2/obj/Debug/net8.0/lab-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd985b38ef3161e28bd9bac589fbb33b8ccf5ff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4e4937527fbc669413034bb34509919fdd50d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
