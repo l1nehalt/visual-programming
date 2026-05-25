642c215287ad88ac9b2a51b2e78bd1151f7f1c18
l1nehalt <alexdenisov656@gmail.com>
2026-05-25 14:03:21 +0300

    pr-15

---
+++ pr-15/CalcWebApp/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalcWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "this is test";
+        }
+
+        public string CheckDate()
+        {
+            return $"today is {DateTime.UtcNow}";
+        }
+
+        public string GetResult(int a, int b, string operation)
+        {
+            switch (operation)
+            {
+                case "Add":
+                    return $"{a + b}";
+
+                case "Sub":
+                    return $"{a - b}";
+
+                case "Mul":
+                    return $"{a * b}";
+
+                case "Div":
+                    return $"{a / b}";
+
+                default:
+                    return "Неизвестная операция";
+            }
+        }
+
+        public string GetInfo()
+        {
+            return $"Denisov Alexey Vladimirovich \n" +
+                $"GitHub: https://github.com/l1nehalt";
+        }
+    }
+}
