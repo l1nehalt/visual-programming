c69fbac0e47b6107f78176f16c5c59c210b37985
l1nehalt <alexdenisov656@gmail.com>
2026-05-26 15:51:19 +0300

    pr-13

---
+++ pr-13/pr-13/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace pr_13
+{
+    public partial class MainPage : ContentPage
+    {
+        private Random _rnd = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        Color GetRandomColor()
+        {
+            return Color.FromRgb(_rnd.Next(0, 256), _rnd.Next(0, 256), _rnd.Next(0, 256));
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            button.BackgroundColor = GetRandomColor();
+
+            double maxWidth = mainLayout.Width - button.Width;
+            double maxHeight = mainLayout.Height - button.Height;
+
+            if (maxWidth <= 0 || maxHeight <= 0) return;
+
+            double randomX = _rnd.NextDouble() * maxWidth;
+            double randomY = _rnd.NextDouble() * maxHeight;
+
+            AbsoluteLayout.SetLayoutFlags(button, AbsoluteLayoutFlags.None);
+
+            AbsoluteLayout.SetLayoutBounds(button, new Rectangle(randomX, randomY, button.Width, button.Height));
+        }
+    }
+}
